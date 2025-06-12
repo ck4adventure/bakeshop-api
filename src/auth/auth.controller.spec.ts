3ccdb0eb10a516dc6ccac8963862afbece8236b7
Christina Kiesz <ckiesz@gmail.com>
2025-06-11 18:38:26 -0700

    update tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthModule } from './auth.module';
+
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
+      imports: [AuthModule],
+			controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: APP_GUARD,
+          useClass: AuthGuard,
+        },
+        {
+          provide: UsersService,
+          useValue: {}, // mock implementation
+        },
+        {
+          provide: JwtService,
+          useValue: { sign: jest.fn(), verifyAsync: jest.fn() }, // mock implementation
+        },
+        {
+          provide: ConfigService,
+          useValue: { get: jest.fn() }, // mock implementation
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
