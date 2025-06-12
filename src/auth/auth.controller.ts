fef5298514cfa3daa37462fc9c444fff326b15aa
Christina Kiesz <ckiesz@gmail.com>
2025-06-11 18:10:42 -0700

    cookies set and unset

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 
-import { Body, Controller, Post,Get, Request, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
+import { Body, Res, Controller, Post, Get, Request, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { Public } from './metadata';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -11,10 +12,35 @@ export class AuthController {
 	@Public()
 	@HttpCode(HttpStatus.OK)
 	@Post('login')
-	signIn(@Body() body: { username: string; password: string }) {
+	async signIn(
+		@Body() body: { username: string; password: string },
+		@Res({ passthrough: true }) res: Response
+	) {
 		// TODO add a handler for undefined body
-		return this.authService.signIn(body.username, body.password);
+		const { access_token } = await this.authService.signIn(body.username, body.password);
+		res.cookie('access_token', access_token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'lax',
+			maxAge: 3600000, // 1 hour, optional
+		});
+		return { status: "ok" }
 	}
+
+	@Public()
+	@Post('logout')
+	signOut(@Res({ passthrough: true }) res: Response) {
+		res.clearCookie('access_token', {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'lax',
+		});
+		return { status: 'signed out' };
+	}
+
+
+
+
 	// @UseGuards(AuthGuard)
 	@Get('profile')
 	getProfile(@Request() req: any) {
