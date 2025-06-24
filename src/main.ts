a22e9d9b001a341791df5aab2537878b08c701d1
Christina Kiesz <ckiesz@gmail.com>
2025-06-24 14:28:47 -0700

    update auth for login

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+
 
 // is an Express platform application by default
 // but we set it explicity
@@ -10,8 +12,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(helmet());
+
+	app.use(cookieParser());
   // TODO set CORS to only trusted origins in prod
-  app.enableCors();
+  app.enableCors({
+		origin: ['http://localhost:5173'], // Replace with your frontend's URL and port
+    credentials: true, // If you need cookies or auth headers
+	});
 
   // TODO swagger api only avail locally
   // swagger setup
