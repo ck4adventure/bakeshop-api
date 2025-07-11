258af5f54c288e20e91b4912b5b8050e3cf17bc1
Christina Kiesz <ckiesz@gmail.com>
2025-07-11 16:46:47 -0700

    add seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -28,9 +28,10 @@ const items_demo_data = [
 // in parallel (smaller datasets only)
 async function main() {
   for (const item of items_demo_data) {
-    await prisma.item.create({
+    const result = await prisma.item.create({
       data: item
     })
+		console.log(result)
   }
 }
 
