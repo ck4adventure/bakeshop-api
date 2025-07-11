54c99763b71f48fb6e219200fca7e07539e0865e
Christina Kiesz <ckiesz@gmail.com>
2025-07-11 16:27:48 -0700

    add seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const items_demo_data = [
+	{ name: "Chocolate Chip Cookies"},
+	{ name: "Brookies"},
+	{ name: "Snickerdoodles"},
+	{ name: "Oatmeal Raisin"},
+]
+
+// createMany
+// async function main() {
+//   await prisma.item.createMany({
+//     data: items_demo_data,
+//     skipDuplicates: true, // optional if you don't want duplicate errors
+//   })
+// }
+
+// looping
+// async function main() {
+//   for (const item of items_demo_data) {
+//     await prisma.item.create({
+//       data: item
+//     })
+//   }
+// }
+
+// in parallel (smaller datasets only)
+async function main() {
+  for (const item of items_demo_data) {
+    await prisma.item.create({
+      data: item
+    })
+  }
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
