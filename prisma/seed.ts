3b61b62fcb3959a0d252f91511934e7274d06cff
Christina Kiesz <ckiesz@gmail.com>
2025-10-16 03:22:32 -0700

    add production sched to db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,11 @@
-import { InventoryReason, PrismaClient } from '@prisma/client'
+import { InventoryReason, PrismaClient, Weekday } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const items_demo_data = [
-  { name: "Chocolate Chip Cookies", slug: "chocolate-chip-cookies" },
-  { name: "Brookies", slug: "brookies" },
-  { name: "Snickerdoodles", slug: "snickerdoodles" },
-  { name: "Oatmeal Raisin", slug: "oatmeal-raisin" },
+	{ name: "Chocolate Chip Cookies", slug: "chocolate-chip-cookies" },
+	{ name: "Brookies", slug: "brookies" },
+	{ name: "Snickerdoodles", slug: "snickerdoodles" },
+	{ name: "Oatmeal Raisin", slug: "oatmeal-raisin" },
 ];
 
 // createMany
@@ -27,12 +27,14 @@ const items_demo_data = [
 
 // in parallel (smaller datasets only)
 async function main() {
-  for (const item of items_demo_data) {
+	for (const item of items_demo_data) {
+
 		// create item
-    const itemResult = await prisma.item.create({
-      data: item,
-    });
-    console.log("item created: ", itemResult);
+		const itemResult = await prisma.item.create({
+			data: item,
+		});
+		console.log("item created: ", itemResult);
+
 		// create batch for item to give it a quantity
 		const qty = itemResult.id * 10;
 		const batchResult = await prisma.inventoryTransaction.create({
@@ -44,13 +46,63 @@ async function main() {
 		});
 		console.log("batch created: ", batchResult);
 
-  }
+		// give item a production schedule
+		// itemId, weekday 0-6, quantity
+		// const schedResult = await prisma.productionSchedule.create({
+		// 	data: {
+		// 		itemId: itemResult.id,
+		// 		quantity: 10,
+		// 		weekday: Weekday.Sunday
+		// 	}
+		// })
+		const schedResult = await prisma.productionSchedule.createMany({
+			data: [
+				{
+					itemId: itemResult.id,
+					quantity: 0,
+					weekday: Weekday.Monday
+				},
+				{
+					itemId: itemResult.id,
+					quantity: 0,
+					weekday: Weekday.Tuesday
+				},
+				{
+					itemId: itemResult.id,
+					quantity: 10,
+					weekday: Weekday.Wednesday
+				},
+				{
+					itemId: itemResult.id,
+					quantity: 10,
+					weekday: Weekday.Thursday
+				},
+				{
+					itemId: itemResult.id,
+					quantity: 15,
+					weekday: Weekday.Friday
+				},
+				{
+					itemId: itemResult.id,
+					quantity: 15,
+					weekday: Weekday.Saturday
+				},
+				{
+					itemId: itemResult.id,
+					quantity: 10,
+					weekday: Weekday.Sunday
+				}
+			]
+		})
+		console.log("production schedule results: ", schedResult)
+
+	}
 }
 
 main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+	.then(() => prisma.$disconnect())
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
